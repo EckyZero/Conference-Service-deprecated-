69018427d801bd1d8616cf3b38879201d264fb6f
Erik Andersen <erik.andersen@parivedasolutions.com>
2020-06-16 19:46:43 -0500

    Last error says that "local: can only be used in a function". Removing local

--- infrastructure/cloudbuild.sh
+++ infrastructure/cloudbuild.sh
@@ -1,5 +1,5 @@
-local APP_NAME=$1
-local CONFIG_FILE=$2
+APP_NAME=$1
+CONFIG_FILE=$2
 
 # Check if it already exists
 set +e
@@ -11,6 +11,6 @@ if [ ${exists} -eq 0 ]; then
     echo ${APP_NAME} exists
     gcloud deployment-manager deployments update ${APP_NAME} --config=${CONFIG_FILE}
 else
-# Run Deployment Manager
-gcloud deployment-manager --project=${PROJECT} deployments create ${NAME} --config=${CONFIG_FILE}
+    # Run Deployment Manager
+    gcloud deployment-manager deployments create ${APP_NAME} --config=${CONFIG_FILE}
 fi
