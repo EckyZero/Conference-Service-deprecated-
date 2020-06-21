52681ae04dcaae2336ef82c74990414ee5ca6a13
Erik Andersen <erik.andersen@parivedasolutions.com>
2020-06-21 03:07:44 -0500

    - added scraping packages - added initial scrape of the general conference topics page for their urls

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cheerio = require('cheerio');
+const axios = require("axios");
+const siteUrl = "https://www.churchofjesuschrist.org/general-conference/topics?lang=eng";
+const siteDomain = "https://www.churchofjesuschrist.org"
 
 var app = express();
 
@@ -12,10 +16,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req,res) {
+app.get('/', async (req,res) => {
     const testResponse  = { field: "Hello", value: "World"};
+    const $ = await fetchData();
+    const topicUrls = $('.lumen-content').find('.lumen-tile a').map((i,el) => { return siteDomain + el.attribs.href; })
+
     return res.status(200).send(testResponse);
 });
+
+const fetchData = async () => {
+    const result = await axios.get(siteUrl);
+    return cheerio.load(result.data);
+};
 
 
 module.exports = app;
